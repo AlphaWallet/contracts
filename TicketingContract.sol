36dca271374da17a192a3d5ebdd4af119ed5b1f0
James Sangalli <j.l.sangalli@gmail.com>
2018-08-08 15:18:26 +0800

    with organiser spawn passTo

--- TicketingContract.sol
+++ TicketingContract.sol
@@ -2,37 +2,37 @@
 
 // ["0x474d542b330000000000000000000000020b5b23d4704d415249524e04050001",
 // "0x474d542b330000000000000000000000020b5b23d4704d415249524e04050002",
-// "0x474d542b330000000000000000000000010a5b291a70504f4c53454e0f050001", 
-// "0x474d542b330000000000000000000000010a5b291a70504f4c53454e0f050002", 
-// "0x474d542b330000000000000000000000020b5b2944a052555345475911050001", 
-// "0x474d542b330000000000000000000000020b5b2944a052555345475911050002", 
+// "0x474d542b330000000000000000000000010a5b291a70504f4c53454e0f050001",
+// "0x474d542b330000000000000000000000010a5b291a70504f4c53454e0f050002",
+// "0x474d542b330000000000000000000000020b5b2944a052555345475911050001",
+// "0x474d542b330000000000000000000000020b5b2944a052555345475911050002",
 // "0x474d542b33000000000000000000000006055b3fae205735335735343a050001",
-// "0x474d542b33000000000000000000000006055b3fae205735335735343a050002", 
+// "0x474d542b33000000000000000000000006055b3fae205735335735343a050002",
 // "0x474d542b330000000000000000000000020b5b4a01e04c36314c36323f050001",
 // "0x474d542b330000000000000000000000020b5b4a01e04c36314c36323f050002",
 // "0x474d542b33000000000000000000000001075b2282f05255534b534101050001",
-// "0x474d542b33000000000000000000000001075b2282f05255534b534101050002", 
-// "0x474d542b33000000000000000000000001075b2282f05255534b534101050003", 
-// "0x474d542b33000000000000000000000001075b2282f05255534b534101050004", 
+// "0x474d542b33000000000000000000000001075b2282f05255534b534101050002",
+// "0x474d542b33000000000000000000000001075b2282f05255534b534101050003",
+// "0x474d542b33000000000000000000000001075b2282f05255534b534101050004",
 // "0x474d542b33000000000000000000000001075b2282f05255534b534101050005",
 // "0x474d542b33000000000000000000000001075b2282f05255534b534101050006",
 // "0x474d542b33000000000000000000000001075b2282f05255534b534101050007",
-// "0x474d542b33000000000000000000000001075b2282f05255534b534101050008", 
+// "0x474d542b33000000000000000000000001075b2282f05255534b534101050008",
 // "0x474d542b33000000000000000000000001075b2282f05255534b534101050009",
 // "0x474d542b33000000000000000000000001075b2282f05255534b53410105000a",
 // "0x474d542b33000000000000000000000001075b2282f05255534b53410105000b",
 // "0x474d542b33000000000000000000000001075b2282f05255534b53410105000c",
 // "0x474d542b33000000000000000000000001075b2282f05255534b53410105000d",
-// "0x474d542b33000000000000000000000001075b2282f05255534b53410105000e", 
-// "0x474d542b33000000000000000000000001075b2282f05255534b53410105000f", 
-// "0x474d542b33000000000000000000000001075b2282f05255534b534101050010", 
-// "0x474d542b33000000000000000000000001075b2282f05255534b534101050011", 
-// "0x474d542b33000000000000000000000001075b2282f05255534b534101050012", 
-// "0x474d542b33000000000000000000000001075b2282f05255534b534101050013", 
-// "0x474d542b33000000000000000000000001075b2282f05255534b534101050014"], 
-// "FIFA WC2018", 
-// "SHANKAI", 
-// "0x0D590124d2fAaBbbdFa5561ccBf778914a50BCca", 
+// "0x474d542b33000000000000000000000001075b2282f05255534b53410105000e",
+// "0x474d542b33000000000000000000000001075b2282f05255534b53410105000f",
+// "0x474d542b33000000000000000000000001075b2282f05255534b534101050010",
+// "0x474d542b33000000000000000000000001075b2282f05255534b534101050011",
+// "0x474d542b33000000000000000000000001075b2282f05255534b534101050012",
+// "0x474d542b33000000000000000000000001075b2282f05255534b534101050013",
+// "0x474d542b33000000000000000000000001075b2282f05255534b534101050014"],
+// "FIFA WC2018",
+// "SHANKAI",
+// "0x0D590124d2fAaBbbdFa5561ccBf778914a50BCca",
 // "0xFE6d4bC2De2D0b0E6FE47f08A28Ed52F9d052A02",
 // "0x2e558Bc42E2e37aB638daebA5CD1062e5b9923De"
 
@@ -58,16 +58,16 @@ contract TicketPro
         if(msg.sender != organiser) revert();
         else _;
     }
-    
+
     modifier payMasterOnly()
     {
         if(msg.sender != paymaster) revert();
         else _;
     }
-    
+
     function() public { revert(); } //should not send any ether directly
 
-     
+
     constructor (
         bytes32[] tickets,
         string nameOfContract,
@@ -113,18 +113,38 @@ contract TicketPro
             delete inventory[seller][index];
         }
         seller.transfer(msg.value);
-        
+
         emit Trade(seller, ticketIndices, v, r, s);
     }
-    
-    function loadNewTickets(bytes32[] tickets) public organiserOnly 
+
+    function loadNewTickets(bytes32[] tickets) public organiserOnly
     {
-        for(uint i = 0; i < tickets.length; i++) 
+        for(uint i = 0; i < tickets.length; i++)
         {
-            inventory[organiser].push(tickets[i]);    
+            inventory[organiser].push(tickets[i]);
         }
     }
-    
+
+    //for new tickets to be created and given over
+    function spawnPassTo(uint256 expiry,
+                    uint256[] tickets,
+                    uint8 v,
+                    bytes32 r,
+                    bytes32 s,
+                    address recipient) public
+    {
+        require(expiry > block.timestamp || expiry == 0);
+        bytes32 message = encodeMessage(0, expiry, tickets);
+        address giver = ecrecover(message, v, r, s);
+        //only the organiser can authorise this
+        require(giver == organiser);
+        for(uint i = 0; i < tickets.length; i++)
+        {
+            inventory[recipient].push(tickets[i]);
+        }
+        emit PassTo(tickets, v, r, s, recipient);
+    }
+
     function passTo(uint256 expiry,
                     uint16[] ticketIndices,
                     uint8 v,
@@ -139,13 +159,13 @@ contract TicketPro
         {
             uint16 index = ticketIndices[i];
             //needs to use revert as all changes should be reversed
-            //if the user doesnt't hold all the tickets 
+            //if the user doesnt't hold all the tickets
             assert(inventory[giver][index] != bytes32(0));
             bytes32 ticket = inventory[giver][index];
             inventory[recipient].push(ticket);
             delete inventory[giver][index];
         }
-        
+
         emit PassTo(ticketIndices, v, r, s, recipient);
     }
 
@@ -205,7 +225,7 @@ contract TicketPro
         return inventory[msg.sender];
     }
 
-    function transfer(address _to, uint16[] ticketIndices) public 
+    function transfer(address _to, uint16[] ticketIndices) public
     {
         for(uint i = 0; i < ticketIndices.length; i++)
         {
@@ -229,7 +249,7 @@ contract TicketPro
             inventory[_to].push(inventory[msg.sender][index]);
             delete inventory[_from][index];
         }
-        
+
         emit TransferFrom(_from, _to, ticketIndices);
     }
 
@@ -238,9 +258,9 @@ contract TicketPro
         selfdestruct(organiser);
     }
 
-    function isStormBirdContract() public pure returns (bool) 
+    function isStormBirdContract() public pure returns (bool)
     {
-        return true; 
+        return true;
     }
 
     function getContractAddress() public view returns(address)
