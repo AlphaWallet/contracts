843f2c3ce7cf96eb99581a0f68d43588c580af25
James Sangalli <j.l.sangalli@gmail.com>
2018-08-21 16:57:04 +0800

    Spawnable pass to (#43)
    * with organiser spawn passTo
    
    * change variable type from bytes32 to uint256 and include custom message encoder for spawnable passTo
    
    * remove redundant vars and function
    
    * Move bitshift to correct place
    
    * loop inner encodings
    
    * shift the other way round
    
    * start from one
    
    * Prevent spawnable double spending with signature check
    
    * Add extra fields to smart contract for event organisers
    
    * Use mapping of signatures (more efficient)
    
    * Use tokenId to get
    
    * seperate into new dir
    
    * following directory structure
    
    * deleting key
    
    * remove .DS_Store & add gitignore
    
    * remove .DS_Store & add gitignore
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * with jb fixes & dir rename
    
    * set interface hashes on deployment

--- spawnable-contract/schema1/TicketingContract.sol
+++ spawnable-contract/schema1/TicketingContract.sol
@@ -1,4 +1,4 @@
-//mainnet: 0xA66A3F08068174e8F005112A8b2c7A507a822335
+//ropsten: 0xCC646F18c7C22F4dF8eBFe7fA45a49F1634bF6aF
 
 // ["0x474d542b330000000000000000000000020b5b23d4704d415249524e04050001",
 // "0x474d542b330000000000000000000000020b5b23d4704d415249524e04050002",
@@ -30,26 +30,47 @@
 // "0x474d542b33000000000000000000000001075b2282f05255534b534101050012", 
 // "0x474d542b33000000000000000000000001075b2282f05255534b534101050013", 
 // "0x474d542b33000000000000000000000001075b2282f05255534b534101050014"], 
-// "FIFA WC2018", 
-// "SHANKAI", 
-// "0x0D590124d2fAaBbbdFa5561ccBf778914a50BCca", 
+// "0xFE6d4bC2De2D0b0E6FE47f08A28Ed52F9d052A02", 
 // "0xFE6d4bC2De2D0b0E6FE47f08A28Ed52F9d052A02",
-// "0x2e558Bc42E2e37aB638daebA5CD1062e5b9923De"
+// "0xFE6d4bC2De2D0b0E6FE47f08A28Ed52F9d052A02",
+// "Hong leong building",
+// "SGP",
+// "HSBC",
+// "HSBC Blockchain Bashathon"
+
+contract ERC165 {
+    /// @notice Query if a contract implements an interface
+    /// @param interfaceID The interface identifier, as specified in ERC-165
+    /// @dev Interface identification is specified in ERC-165. This function
+    ///  uses less than 30,000 gas.
+    /// @return `true` if the contract implements `interfaceID` and
+    ///  `interfaceID` is not 0xffffffff, `false` otherwise
+    function supportsInterface(bytes4 interfaceID) external view returns (bool);
+}
 
 pragma solidity ^0.4.17;
-contract TicketPro
+contract TicketPro is ERC165
 {
-    mapping(address => bytes32[]) inventory;
     uint16 ticketIndex = 0; //to track mapping in tickets
+    mapping(address => uint256[]) inventory;
+    mapping(bytes32 => bool) signatureChecked; 
     address organiser;
     address paymaster;
     uint numOfTransfers = 0;
     string public name;
-    string public symbol;
     uint8 public constant decimals = 0; //no decimals as tickets cannot be split
+    bool isExpired;
+    string state;
+    string street; 
+    string building;
+    string symbol;
+    bytes4 balHash = bytes4(keccak256('balanceOf(address)'));
+    bytes4 tradeHash = bytes4(keccak256('trade(uint256,uint16[],uint8,bytes32,bytes32)'));
+    bytes4 passToHash = bytes4(keccak256('passTo(uint256,uint16[],uint8,bytes32,bytes32,address)'));
+    bytes4 spawnPassToHash = bytes4(keccak256('spawnPassTo(uint256,uint256[],uint8,bytes32,bytes32,address)'));
 
-    event Transfer(address indexed _to, uint16[] _indices);
-    event TransferFrom(address indexed _from, address indexed _to, uint16[] _indices);
+    event Transfer(address indexed _to, uint256 count);
+    event TransferFrom(address indexed _from, address indexed _to, uint256 count);
     event Trade(address indexed seller, uint16[] ticketIndices, uint8 v, bytes32 r, bytes32 s);
     event PassTo(uint16[] ticketIndices, uint8 v, bytes32 r, bytes32 s, address indexed recipient);
 
@@ -69,23 +90,59 @@ contract TicketPro
 
      
     constructor (
-        bytes32[] tickets,
-        string nameOfContract,
-        string symbolForContract,
+        uint256[] tickets,
         address organiserAddr,
         address paymasterAddr,
-        address recipientAddr) public
+        address recipientAddr,
+        string streetName,
+        string buildingName,
+        string stateName,
+        string symbolName,
+        string contractName) public
     {
-        name = nameOfContract;
-        symbol = symbolForContract;
         organiser = organiserAddr;
         paymaster = paymasterAddr;
         inventory[recipientAddr] = tickets;
+        street = streetName;
+        building = buildingName;
+        state = stateName;
+        symbol = symbolName;
+        name = contractName;
     }
-
-    function getDecimals() public pure returns(uint)
+    
+    function supportsInterface(bytes4 interfaceID) external view returns (bool) 
     {
-        return decimals;
+        if(interfaceID == balHash 
+        || interfaceID == tradeHash 
+        || interfaceID == passToHash 
+        || interfaceID == spawnPassToHash) return true;
+        return false;
+    }
+    
+    function checkExpired(uint256 tokenId) public view returns(bool) 
+    {
+        return true;
+    }
+    
+    function getStreet(uint256 tokenId) public view returns(string) 
+    {
+        return street;
+    }
+    
+    function getBuilding(uint256 tokenId) public view returns(string) 
+    {
+        return building;
+    }
+    
+    function getState(uint256 tokenId) public view returns(string) 
+    {
+        return state;
+    }
+    
+    function setExpired(uint256 tokenId) public 
+    {
+        require(msg.sender == organiser);
+        isExpired = true;
     }
 
     // example: 0, [3, 4], 27, "0x9CAF1C785074F5948310CD1AA44CE2EFDA0AB19C308307610D7BA2C74604AE98", "0x23D8D97AB44A2389043ECB3C1FB29C40EC702282DB6EE1D2B2204F8954E4B451"
@@ -107,7 +164,7 @@ contract TicketPro
         for(uint i = 0; i < ticketIndices.length; i++)
         { // transfer each individual tickets in the ask order
             uint16 index = ticketIndices[i];
-            assert(inventory[seller][index] != bytes32(0)); // 0 means ticket gone.
+            assert(inventory[seller][index] != uint256(0)); // 0 means ticket gone.
             inventory[msg.sender].push(inventory[seller][index]);
             // 0 means ticket gone.
             delete inventory[seller][index];
@@ -117,14 +174,35 @@ contract TicketPro
         emit Trade(seller, ticketIndices, v, r, s);
     }
     
-    function loadNewTickets(bytes32[] tickets) public organiserOnly 
+    function loadNewTickets(uint256[] tickets) public organiserOnly 
     {
         for(uint i = 0; i < tickets.length; i++) 
         {
             inventory[organiser].push(tickets[i]);    
         }
     }
-    
+
+    //for new tickets to be created and given over
+    //this requires a special magic link format with tokenids inside rather than indicies
+    function spawnPassTo(uint256 expiry,
+                    uint256[] tickets,
+                    uint8 v,
+                    bytes32 r,
+                    bytes32 s,
+                    address recipient) public
+    {
+        require(expiry > block.timestamp || expiry == 0);
+        bytes32 message = encodeMessageSpawnable(0, expiry, tickets);
+        address giver = ecrecover(message, v, r, s);
+        //only the organiser can authorise this
+        require(giver == organiser);
+        require(!signatureChecked[s]);
+        for(uint i = 0; i < tickets.length; i++)
+        {
+            inventory[recipient].push(tickets[i]);
+        }
+    }
+
     function passTo(uint256 expiry,
                     uint16[] ticketIndices,
                     uint8 v,
@@ -140,8 +218,8 @@ contract TicketPro
             uint16 index = ticketIndices[i];
             //needs to use revert as all changes should be reversed
             //if the user doesnt't hold all the tickets 
-            assert(inventory[giver][index] != bytes32(0));
-            bytes32 ticket = inventory[giver][index];
+            assert(inventory[giver][index] != uint256(0));
+            uint256 ticket = inventory[giver][index];
             inventory[recipient].push(ticket);
             delete inventory[giver][index];
         }
@@ -181,12 +259,41 @@ contract TicketPro
         return keccak256(message);
     }
 
-    function name() public view returns(string)
+    //must also sign in the contractAddress
+    function encodeMessageSpawnable(uint value, uint expiry, uint256[] tickets)
+        internal view returns (bytes32)
     {
-        return name;
+        bytes memory message = new bytes(84 + tickets.length * 32);
+        address contractAddress = getContractAddress();
+        for (uint i = 0; i < 32; i++)
+        {   // convert bytes32 to bytes[32]
+            // this adds the price to the message
+            message[i] = byte(bytes32(value << (8 * i)));
+        }
+
+        for (i = 0; i < 32; i++)
+        {
+            message[i + 32] = byte(bytes32(expiry << (8 * i)));
+        }
+
+        for(i = 0; i < 20; i++)
+        {
+            message[64 + i] = byte(bytes20(bytes20(contractAddress) << (8 * i)));
+        }
+
+        for (i = 0; i < tickets.length; i++)
+        {
+            message[84 + i * 32 ] = byte(tickets[i]);
+            // convert uint256[] to bytes
+            for (uint j = 1; j < 32; j++)
+            {
+                message[84 + i * 32 + j] = byte(tickets[i] = tickets[i] >> 8);
+            }
+        }
+        return keccak256(message);
     }
 
-    function symbol() public view returns(string)
+    function getSymbol() public view returns(string)
     {
         return symbol;
     }
@@ -196,12 +303,12 @@ contract TicketPro
         return numOfTransfers;
     }
 
-    function balanceOf(address _owner) public view returns (bytes32[])
+    function balanceOf(address _owner) public view returns (uint256[])
     {
         return inventory[_owner];
     }
 
-    function myBalance() public view returns(bytes32[]){
+    function myBalance() public view returns(uint256[]){
         return inventory[msg.sender];
     }
 
@@ -210,12 +317,12 @@ contract TicketPro
         for(uint i = 0; i < ticketIndices.length; i++)
         {
             uint index = uint(ticketIndices[i]);
-            assert(inventory[msg.sender][index] != bytes32(0));
+            assert(inventory[msg.sender][index] != uint256(0));
             //pushes each element with ordering
             inventory[_to].push(inventory[msg.sender][index]);
             delete inventory[msg.sender][index];
         }
-        emit Transfer(_to, ticketIndices);
+        emit Transfer(_to, ticketIndices.length);
     }
 
     function transferFrom(address _from, address _to, uint16[] ticketIndices)
@@ -224,13 +331,13 @@ contract TicketPro
         for(uint i = 0; i < ticketIndices.length; i++)
         {
             uint index = uint(ticketIndices[i]);
-            assert(inventory[_from][index] != bytes32(0));
+            assert(inventory[_from][index] != uint256(0));
             //pushes each element with ordering
             inventory[_to].push(inventory[msg.sender][index]);
             delete inventory[_from][index];
         }
         
-        emit TransferFrom(_from, _to, ticketIndices);
+        emit TransferFrom(_from, _to, ticketIndices.length);
     }
 
     function endContract() public organiserOnly
