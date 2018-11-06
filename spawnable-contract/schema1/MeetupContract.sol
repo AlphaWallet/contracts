fbb81d63592533e8da03b58f3cf6c16a5af97516
colourful-land <a@colourful.land>
2018-11-06 15:04:41 +0800

    - remove the ticket indices part of the message. - this contact is not ERC165 compatible for now, delete ERC165 related stuff.

--- spawnable-contract/schema1/MeetupContract.sol
+++ spawnable-contract/schema1/MeetupContract.sol
@@ -1,15 +1,5 @@
-contract ERC165 {
-    /// @notice Query if a contract implements an interface
-    /// @param interfaceID The interface identifier, as specified in ERC-165
-    /// @dev Interface identification is specified in ERC-165. This function
-    ///  uses less than 30,000 gas.
-    /// @return `true` if the contract implements `interfaceID` and
-    ///  `interfaceID` is not 0xffffffff, `false` otherwise
-    function supportsInterface(bytes4 interfaceID) external view returns (bool);
-}
-
 pragma solidity ^0.4.17;
-contract TicketPro //is ERC165
+contract TicketPro
 {
     mapping(address => uint256[]) inventory;
 	uint256[] spawnedTickets;
@@ -263,10 +253,6 @@ contract TicketPro //is ERC165
             {
                 message[84 + i * 32 + 31 - j] = byte(tickets[i] = tickets[i] >> 8);
             }
-        }
-        for(i=0; i < _tickets.length; i++)
-        {
-            tickets[i] = _tickets[i];
         }
         return keccak256(message);
     }
